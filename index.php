11f25ba5d15f50e2dabd7cb167e851187478737f
Karhan1 <rananuryuksel@gmail.com>
2026-04-27 10:55:03 +0300

    1. Gun: Proje iskeleti ve Hakkinda sayfasi tamamlandi

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <title>Hakkımda | Web Projesi</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">B251210081</a>
+            <div class="navbar-nav ms-auto">
+                <a class="nav-link active" href="index.php">Hakkında</a>
+                <a class="nav-link" href="ozgecmis.php">Özgeçmiş</a>
+                <a class="nav-link" href="sehrim.php">Şehrim</a>
+                <a class="nav-link" href="iletisim.php">İletişim</a>
+                <a class="nav-link" href="login.php">Login</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container mt-5 text-center">
+        <h1>merhaba , Ben Rana Nur Yüksel</h1>
+        <img src="img/profilim.jpg" class="img-fluid rounded-circle shadow my-4" style="width: 200px;" alt="Benim Fotoğrafım">
+        <p class="lead">Bu proje Sakarya Üniversitesi Web Teknolojileri dersi için hazırlanmıştır.</p>
+        <div class="row justify-content-center mt-4">
+    <div class="col-md-8 text-start">
+        <h3>Hobilerim </h3>
+        <ul>
+            <li>karakalem çalışmaları yapmak.</li>
+            <li>müzik dinleyerek yürüyüş yapmak.</li>
+            <li>kitap okumak ve yazmak.</li>
+        </ul>
+        
+        <h3 class="mt-4">Bağlantılar</h3>
+        <p>Beni sosyal medyadan takip edebilirsiniz:</p>
+        <a href="https://github.com/Karhan1" class="btn btn-outline-dark">GitHub Profili</a>
+        <a href="https://linkedin.com/in/Rananuryüksel" class="btn btn-outline-primary">LinkedIn</a>
+    </div>
+</div>
+        <div class="alert alert-info">Bugün 1. Gün: Dosya yapısı ve ana sayfa iskeleti kuruldu.</div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
